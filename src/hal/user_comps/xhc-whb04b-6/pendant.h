1f79c042bfeae1df2375fb07ccc42036bcb4bff8
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-02-16 23:04:37 +0100

    whb04b: Pass arguments properly
    Fixes step 5/10 enable/disable

--- src/hal/user_comps/xhc-whb04b-6/pendant.h
+++ src/hal/user_comps/xhc-whb04b-6/pendant.h
@@ -391,6 +391,7 @@ public:
     ~FeedRotaryButton();
     virtual bool setKeyCode(const KeyCode& keyCode) override;
     void setStepMode(HandwheelStepmodes::Mode stepMode);
+    void setStepMode_5_10(bool enable);
     HandwheelStepmodes::Mode stepMode() const;
     float stepSize() const;
     bool isPermitted() const override;
@@ -586,7 +587,6 @@ private:
     //! if in Lead mode: if true jog wheel changes the spindle speed, changes the feed override otherwise
     bool         mIsLeadModeSpindle = false;
     bool         mIsLeadModeFeed = false;
-    bool         mIsStepMode_5_10 = false;
 
     float mScale;
     float mMaxVelocity;
