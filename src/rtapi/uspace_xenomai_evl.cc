5a5f2249813b98e2e88673561a850ba0caae7b4b
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-07-13 15:23:52 +0200

    Xenomai EVL: Remove uneeded debug print

--- src/rtapi/uspace_xenomai_evl.cc
+++ src/rtapi/uspace_xenomai_evl.cc
@@ -98,7 +98,6 @@ struct XenomaiApp : RtapiApp {
         {
             WithRoot r;
             /* Attach to the core. */
-            rtapi_print("linuxcnc-task:%d\n", gettid());
             int tfd = evl_attach_self("linuxcnc-thread:%d", gettid());
             if (tfd < 0){
                 rtapi_print("evl_attach_self() failed ret %i errno %i\n", tfd, errno);
